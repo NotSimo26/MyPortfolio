774f664976978fac3c664a4ce4fef0851b3a4468
= <=>
2026-02-02 17:11:22 +0100

    test

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ async function initViewer() {
         scene.add(backLight);
         
         const mtlLoader = new MTLLoader();
-        mtlLoader.setResourcePath('/models');
+        mtlLoader.setResourcePath('/models/');
         
         // Carica il file MTL
         mtlLoader.load('/models/SimoAvatar.mtl', (materials) => {
